68cd1450d065b6ca1c738a0975bf727352995a63
Mescht <mescht93@gmail.com>
2016-11-10 11:14:50 +0100

    Add communication interface for mobile part

--- mobile/src/main/java/com/ooe/fh/liftme/UI/Activity/MainActivity.java
+++ mobile/src/main/java/com/ooe/fh/liftme/UI/Activity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.ooe.fh.liftme.UI.Activity;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,18 +10,33 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
 import com.ooe.fh.liftme.Models.OverviewTraining_Listitem_Model;
 import com.ooe.fh.liftme.R;
 import com.ooe.fh.liftme.UI.Adapters.SectionsPagerAdapter;
 import com.ooe.fh.liftme.UI.Layout.Elements.CustomViewPager;
 import com.ooe.fh.liftme.application.AppClass;
+import com.ooe.fh.liftme.utils.Listeners;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, MessageApi.MessageListener, DataApi.DataListener {
 
     @Bind(R.id.tablayout)
     TabLayout tablayout;
@@ -33,6 +50,10 @@ public class MainActivity extends AppCompatActivity {
     //Adapters
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
+    private String mNodeId;
+    private GoogleApiClient mGoogleApiClient;
+
+    private Listeners.OnMessageReceivedListener messageListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,8 +90,28 @@ public class MainActivity extends AppCompatActivity {
         mViewPager.setCurrentItem(1);
 
         AppClass.overviewTraining_Listitem_models = new ArrayList<OverviewTraining_Listitem_Model>();
+
+        initializeGoogleApiClient();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mGoogleApiClient.connect();
+        getBestNode();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mGoogleApiClient != null) {
+            Wearable.DataApi.removeListener(mGoogleApiClient, this);
+            Wearable.MessageApi.removeListener(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -91,5 +132,77 @@ public class MainActivity extends AppCompatActivity {
 
     public CustomViewPager getmViewPager(){
         return mViewPager;
+    }
+
+    private void initializeGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    public GoogleApiClient getGoogleApiClient() {
+        return mGoogleApiClient;
+    }
+
+    public String getNodeId() {
+        return mNodeId;
+    }
+
+    public void setOnMessageReceivedListener(Listeners.OnMessageReceivedListener messageListener) {
+        this.messageListener = messageListener;
+    }
+
+    private void getBestNode() {
+
+        // This must be an asynchronous call with callback
+        // Otherwise .getConnectedNodes().await will return it immediately,
+        // but must not called on the UI thread
+        Wearable.NodeApi.getConnectedNodes(mGoogleApiClient)
+                .setResultCallback(new ResultCallback<NodeApi.GetConnectedNodesResult>() {
+            @Override
+            public void onResult(@NonNull NodeApi.GetConnectedNodesResult getConnectedNodesResult) {
+
+                // Just get the first connected node
+                // We assume that there is just 1 Wearable connected
+                List<Node> nodes = getConnectedNodesResult.getNodes();
+                if (nodes.size() > 0) {
+                    mNodeId = nodes.get(0).getId();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Wearable.DataApi.addListener(mGoogleApiClient, this);
+        Wearable.MessageApi.addListener(mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) { }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) { }
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEventBuffer) {
+
+        // TODO Handle data synchronisation from watch
+        for (DataEvent event : dataEventBuffer) {
+            if (event.getType() == DataEvent.TYPE_CHANGED) {
+                // DataItem changed
+                DataItem item = event.getDataItem();
+            }
+        }
+    }
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+
+        if (messageListener != null) {
+            messageListener.onMessageReceived(messageEvent);
+        }
     }
 }
